36c924f20afabbbf47c5cd1c1ba68e283941f3a4
Bathala Mohan <mohan.bathala@adesso.in>
2025-12-02 09:14:59 +0530

    updated the test code

--- GivenNumberInWords.java
+++ GivenNumberInWords.java
@@ -136,6 +136,9 @@ public class GivenNumberInWords {
     }
 
 	public static void main(String[] args) {
+		String query = "SELECT * FROM users WHERE id=" + id;
+        System.out.println(query);
+        query = null;
 		convertToWords("6119891");
 		convertToWords("999,999,999");
 		convertToWords("asdfasdf");
